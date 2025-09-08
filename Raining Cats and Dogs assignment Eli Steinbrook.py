8fcb7a4ac7e0424e7935a2acb7d3c967071b3124
elisteinbrook <esteinbrook1@ivytech.edu>
2025-09-08 00:34:43 -0400

    Add files via upload

---
+++ Raining Cats and Dogs assignment Eli Steinbrook.py
@@ -0,0 +1,12 @@
+# Set variable
+STRING_CATS = "cats"
+
+# Set variable
+STRING_DOGS = "dogs."
+
+
+# Set variable
+finalStatement = "It is raining " + STRING_CATS + " and " + STRING_DOGS
+
+# Print
+print(finalStatement)
